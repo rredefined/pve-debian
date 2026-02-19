42b3b048c12ce72b4a1da3c5ecddc6667f85a62c
Eiro.tf <freakgaming2009@gmail.com>
2026-02-19 13:22:09 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
-apt install git -y && git clone https://github.com/rredefined/pve-debian.git
-cd pve-debian
+#!/bin/bash
+
+# Clone the repository
+git clone https://github.com/rredefined/pve-debian.git
+
+# Move into the directory
+cd pve-debian || { echo "Failed to enter directory"; exit 1; }
+
+# Make install script executable
 chmod +x install.sh
+
+# Run the installer
 ./install.sh
